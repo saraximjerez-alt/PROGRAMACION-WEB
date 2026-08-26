ed36e98cde66ea072ef858158456730aef910040
saraximjerez-alt <saraximjerez@gmail.com>
2026-08-26 18:45:49 -0500

    Create NOMPERSONA.java

---
+++ NOMPERSONA.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class NOMPERSONA {
+    private String nombre;
+    private int edad;
+    private String documento;
+
+    // Constructor
+    public NOMPERSONA(String nombre, int edad, String documento) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.documento = documento;
+    }
+
+    // Getters y Setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public int getEdad() { return edad; }
+    public void setEdad(int edad) { this.edad = edad; }
+
+    public String getDocumento() { return documento; }
+    public void setDocumento(String documento) { this.documento = documento; }
+
+    // Método mostrarInformacion
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre + " | Edad: " + edad + " | Documento: " + documento);
+    }
+}
+
+
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        NOMPERSONA p1 = new NOMPERSONA("Juan", 20, "12345678");
+        p1.mostrarInformacion();
+    }
+}
